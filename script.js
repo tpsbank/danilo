f17e283aa741027c44670072a71a465164f2d43e
TPS Bank <danilo.soru@tempocasa.com>
2026-05-07 14:06:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('.actions div').forEach(btn => {
-  btn.addEventListener('click', () => {
-    btn.style.transform = "scale(0.95)";
-    setTimeout(() => {
-      btn.style.transform = "scale(1)";
-    }, 150);
-  });
-});
+
